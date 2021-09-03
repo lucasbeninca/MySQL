4b67a0dd0f861e1072b5912c57a7a07d645cc5a1
lucasbeninca <lebbeninca@gmail.com>
2021-09-03 15:10:04 -0300

    avançando mais no curso de sql

---
+++ INSERT TABELA.sql
@@ -0,0 +1,32 @@
+USE SUCOS;
+
+INSERT INTO tb_produtos(
+PRODUTO,
+NOME,
+EMBALAGEM,
+TAMANHO,
+SABOR,
+PRECO_LISTA
+) VALUES ('1040107','LIGHT -3 50ML - melancia','lata','350 ml','melancia', 4.56 );
+
+INSERT INTO tb_produtos (
+PRODUTO,  NOME, EMBALAGEM, TAMANHO, SABOR,
+PRECO_LISTA) VALUES
+('544931', 'Frescor do Verão - 350 ml - Limão', 'PET', '350 ml','Limão',3.20);
+
+INSERT INTO tb_produtos (
+PRODUTO,  NOME, EMBALAGEM, TAMANHO, SABOR,
+PRECO_LISTA) VALUES
+('1078680', 'Frescor do Verão - 470 ml - Manga', 'Lata', '470 ml','Manga',5.18);
+
+INSERT INTO tb_produtos (
+PRODUTO,  NOME, EMBALAGEM, TAMANHO, SABOR,
+PRECO_LISTA) VALUES
+('5449321', 'Frescor do Verão - 360 ml - abacate', 'PET', '360 ml','Limão',3.20);
+
+INSERT INTO tb_produtos (
+PRODUTO,  NOME, EMBALAGEM, TAMANHO, SABOR,
+PRECO_LISTA) VALUES
+('10786280', 'Frescor do Verão - 480 ml - maça', 'Lata', '470 ml','maça',5.18);
+
+
