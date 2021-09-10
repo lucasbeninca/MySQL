4ca995300bd6711e9c3075db122d9dde0b4f1372
lucasbeninca <lebbeninca@gmail.com>
2021-09-10 11:21:39 -0300

    uso do like

--- USANDO IN.sql
+++ USANDO IN.sql
@@ -3,4 +3,6 @@ use sucos_vendas;
 select * from itens_notas_fiscais;
 
 select * from tabela_de_clientes where cidade in ('São Paulo', 'Rio de Janeiro') 
-and idade > 16 and idade < 22;
+and (idade > 16 and idade < 22);
+
+select * from tabela_de_clientes where nome like '%mattos';
